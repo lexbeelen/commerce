e6f3c64d3ae204c099e204456a8b9f74a5cdd4cf
Lex Beelen <lex@lexbeelen.nl>
2024-04-03 22:07:54 +0200

    Init

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  i18n: {
+    locales: ["default", "en-us", "nl-nl"],
+    defaultLocale: "default",
+    localeDetection: true,
+  },
+  trailingSlash: false,
 };
 
 export default nextConfig;
